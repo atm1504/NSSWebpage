09e5c0ee293d23f58f5d5d581d12d8f3707d68b0
Amartya Mondal <hayyoulistentome@gmail.com>
2019-04-06 20:47:47 +0530

    Changed username

--- js/login.js
+++ js/login.js
@@ -2,14 +2,14 @@ var wrong=$('<div class="alert alert-danger" role="alert">Wrong username or pass
 var success=$('<div class="alert alert-success" role="alert">Successful!</div>');
 $('#stud_button').click(function(e){
     e.preventDefault();
-    if(($('#stud_user').val()!="ashwani")||($('#stud_pass').val()!="ashwani")){
+    if(($('#stud_user').val()!="username")||($('#stud_pass').val()!="password")){
     $('#stud_feedback').empty();
     $('#stud_feedback').append(wrong);
     setTimeout(function(){ 
         $('#stud_feedback').empty();
     }, 3000);
     }
-    if(($('#stud_user').val()=="ashwani")&&($('#stud_pass').val()=="ashwani")){
+    if(($('#stud_user').val()=="username")&&($('#stud_pass').val()=="password")){
     $('#stud_feedback').empty();
     $('#stud_feedback').append(success);
     setTimeout(function(){ 
@@ -22,14 +22,14 @@ $('#stud_button').click(function(e){
 });
 $('#ment_button').click(function(e){
     e.preventDefault();
-    if(($('#ment_user').val()!="ashwani")||($('#ment_pass').val()!="ashwani")){
+    if(($('#ment_user').val()!="username")||($('#ment_pass').val()!="password")){
     $('#ment_feedback').empty();
     $('#ment_feedback').append(wrong);
     setTimeout(function(){ 
         $('#ment_feedback').empty();
     }, 3000);
     }
-    if(($('#ment_user').val()=="ashwani")&&($('#ment_pass').val()=="ashwani")){
+    if(($('#ment_user').val()=="username")&&($('#ment_pass').val()=="password")){
     $('#ment_feedback').empty();
     $('#ment_feedback').append(success);
     url = "https://docs.google.com/spreadsheets/d/1X_ExfTu0i81_QbuhP6BqkUn29B_zVumqU4aLcIQn59k/edit?usp=sharing";
